751bab4063611f2cb12647f2d892a5e53a490632
diedona <diedona@gmail.com>
2024-10-29 08:18:54 -0300

    updating score

--- scripts/components/Score/Score.gd
+++ scripts/components/Score/Score.gd
@@ -1,14 +1,17 @@
 extends Node2D
 class_name Score
 
-@export var label: Label;
+@export var label_score: Label;
 
 var _score: int = 0;
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	assert(label != null, "label is empty");
+	assert(label_score != null, "label_score is null");
 
 func on_coin_collected() -> void:
 	_score += 1;
-	print_debug("score: ", _score);
+	update_score();
+
+func update_score() -> void:
+	label_score.text = str(_score);
